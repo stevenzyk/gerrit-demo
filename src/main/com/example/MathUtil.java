510df3930a9e1069ab9a4170576ff3929e3e34f7
root <root@localhost.localdomain>
2012-06-25 14:02:06 +0800

    import project file

---
+++ src/main/com/example/MathUtil.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class MathUtil {
+	
+	int add(int value1, int value2) {
+
+		return value1 + value2;
+
+		}
+
+		int subtract(int value1, int value2) {
+
+		return value1 - value2;
+
+		}
+
+		int multiply(int value1, int value2) {
+
+		return value1 * value2;
+
+		}
+
+		int divide(int value1, int value2) {
+
+		return value1 / value2;
+
+		}
+
+}
